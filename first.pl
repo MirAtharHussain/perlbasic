19391920a54ca90052d3d91a5faf7d9ac30e3e42
MirAtharHussain <Miratharhussain52@gmail.com>
2020-08-12 12:43:34 +0530

    [Hussain]ADD:Using switch case to perform basic arithmetic operations using functions

--- first.pl
+++ first.pl
@@ -1,23 +1,30 @@
 #!/usr/bin/perl
 
+ 
+
+sub arithmetic{
+
 print "Enter two digits";
 my $a=<STDIN>;
 my $b=<STDIN>;
 
-use experimental qw(switch say);  
+use experimental qw(switch say); 
 
 print "Choose any option from 1-5";
 chomp( my $c = <> );  
 
 given($c){
- when (1) { say 'Is', $a+$b ;}  
- when (2) { say 'It is $a-$b' ;}  
- when (3) { say 'It is $a*$b' ;}
- when (4) { say 'It is $a%$b' ;}
- when (5) { say 'It is $a/$b' ;}  
+ when (1) { say 'Addition of numbers is', $a+$b ;}  
+ when (2) { say 'Subtraction of number is', $a-$b ;}  
+ when (3) { say 'Multiplication  is', $a*$b ;}
+ when (4) { say 'Modulud is', $a%$b ;}
+ when (5) { say 'division is', $a/$b ;}  
  default { say 'Enter btw 1 - 5';}  
 
 }
+}
+
+arithmetic();
 
 
 
