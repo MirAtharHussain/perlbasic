9239e326b700225cffd700c9d8d2ff7000768293
MirAtharHussain <Miratharhussain52@gmail.com>
2020-08-12 11:50:19 +0530

    [Hussain]ADD:Using switch case to perform basic arithmetic operations without functions

--- first.pl
+++ first.pl
@@ -4,18 +4,18 @@ print "Enter two digits";
 my $a=<STDIN>;
 my $b=<STDIN>;
 
-use Switch;
+use experimental qw(switch say);  
 
 print "Choose any option from 1-5";
-my $c=<STDIN>;
+chomp( my $c = <> );  
 
-switch($c){
-case 1	{print "Addition is: $a+$b"}
-case 2	{print "Subtraction is: ", $a - $b, "\n";}
-case 3	{print "Multiplication is: ", $a * $b, "\n";}
-case 4	{print "Modulus is: ", $a % $b, "\n";}
-case 5	{print "Division is: ", $a / $b, "\n";}
-else	{print "Enter number btw 1 to 5"}
+given($c){
+ when (1) { say 'Is', $a+$b ;}  
+ when (2) { say 'It is $a-$b' ;}  
+ when (3) { say 'It is $a*$b' ;}
+ when (4) { say 'It is $a%$b' ;}
+ when (5) { say 'It is $a/$b' ;}  
+ default { say 'Enter btw 1 - 5';}  
 
 }
 
